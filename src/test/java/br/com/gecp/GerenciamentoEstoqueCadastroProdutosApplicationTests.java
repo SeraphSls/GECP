d19a327acb8496045e923c807938fb461e4a203a
Welber Serafim <serafimwelber@gmail.com>
2021-05-11 22:42:47 -0300

    commit inicial do projto

---
+++ src/test/java/br/com/gecp/GerenciamentoEstoqueCadastroProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gecp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoEstoqueCadastroProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
